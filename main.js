551bf4e35085bdd725243772c3588d8c5690da86
Madhur Shrimal <madhur.shrimal@gmail.com>
2014-09-06 01:26:22 -0400

    Added board and click event

--- main.js
+++ main.js
@@ -1,44 +1,61 @@
 ;(function() {
-
-})();
-$(document).ready(function() {
-	jQuery('#submit').unbind('click').click(function(e){
-		e.preventDefault();
-		var rows = jQuery('input#rows').val();
-		var cols = jQuery('input#cols').val();
-		var container = jQuery("div#tile-wrapper");
-		makeGame(container, rows, cols);
-	})
-});
-var setColor= function (row, col, color){
-	var currTile = getTile(row, col);
-	currTile.removeClass('red')
-		.removeClass('blue')
-		.removeClass('green')
-		.addClass(color);
-
-} 
-
-var getTile = function (row , col){
-	return jQuery('.tile-row:nth(' + row + ') > .tile:nth('
-		 + col + ')');
-}
-var makeGame = function (container, rows, cols){
-	jQuery('form#args').detach();
-	var isValid = rows > 0 && cols > 0;
-	jQuery('h1.green').text(isValid ? 1 : 0);
-	jQuery('h1.blue').text(isValid ? rows*cols - 1 : 0);
-	$curTileRow = jQuery("<div class='tile-row'></div>");
-	$eachTile = jQuery("<div class='tile'></div>");
-	var boardColors = [];
-	for(curRow in rows){
-		boardColors[curRow] = [];
-		container.append($curTileRow);
-		for(curCol in cols){
-			console.log(curCol);
-			container.append($eachTile);
-			boardColors[curRow][curCol] = 'blue';
-			setColor(curRow, curCol, 'blue');
-		}
+	var Board = {
+		boardColors = [],
+		rows : 0,
+		cols : 0
 	}
-};
+
+	$(document).ready(function() {
+		jQuery('#submit').unbind('click').click(function(e){
+			e.preventDefault();
+			var rows = jQuery('input#rows').val();
+			var cols = jQuery('input#cols').val();
+			Board.rows = rows;
+			Board.cols = cols;
+			var container = jQuery("div#tile-wrapper");
+			makeGame(container, rows, cols);
+		});
+
+		jQuery('div.tile').click(function (){
+			var clickedTile = jQuery(this);
+			if(clickedTile.hasClass('green')){
+				var clickedRowNum = clickedTile.parent().index();
+				var clickedColNum = clickedTile.index();
+			}
+		});
+	});
+	var setColor = function (row, col, color){
+		var currTile = getTile(row, col);
+		currTile.removeClass('red')
+			.removeClass('blue')
+			.removeClass('green')
+			.addClass(color);
+
+		Board.boardColors[row][col] = color;
+
+	} 
+
+	var getTile = function (row , col){
+		return jQuery('.tile-row:nth(' + row + ') > .tile:nth('
+			 + col + ')');
+	}
+	var makeGame = function (container, rows, cols){
+		jQuery('form#args').detach();
+		var isValid = rows > 0 && cols > 0;
+		jQuery('h1.green').text(isValid ? 1 : 0);
+		jQuery('h1.blue').text(isValid ? rows*cols - 1 : 0);
+		//$curTileRow = jQuery("<div class='tile-row'></div>");
+		//$eachTile = jQuery("<div class='tile'></div>");
+		
+		for(var curRow = 0; curRow < rows; curRow++){
+			boardColors[curRow] = [];
+			container.append("<div class='tile-row'></div>");
+			for(var curCol = 0; curCol < cols; curCol++){
+				console.log(curCol);
+				container.find('div.tile-row:nth(' + curRow + ')').append("<div class='tile'></div>");
+				setColor(curRow, curCol, 'blue');
+			}
+		}
+		setColor(0,0,'green')
+	};
+})();
